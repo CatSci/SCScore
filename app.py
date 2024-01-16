595f0c8d5dd5e5a3a3a89c9738bbb139f548446d
Pavel Elagin <pavel.elagin@catsci.com>
2024-01-16 23:39:08 +0000

    added comments and remarks

--- app.py
+++ app.py
@@ -11,16 +11,23 @@ st.title('SCR-02: SCScore')  # Replace with your script name
 
 # Brief description
 st.markdown('''
-    description will be provided later
+    This script provides a relative synthetic complexity of a molecule based on 12M reactions taken from Reaxys. The score ranges from 0 to 5, with higher values indicating a more challenging molecule.
+    
+    More information are in related paper [SCScore](https://pubs.acs.org/doi/10.1021/acs.jcim.7b00622)
     ''')
+
+st.warning('For script you need input chirality specified SMILES, not any other [types](https://ics.uci.edu/~dock/manuals/DaylightTheoryManual/theory.smiles.html). Daylight SMILES from ChemDoodle and Isomeric are ok', icon="⚠️")
 
 model = SCScorer()
 model.restore()
 
-molecule = st.text_input('Isomeric SMILES')
+molecule = st.text_input(' Please provide **Isomeric** SMILES')
 (smi, score) = model.get_score_from_smi(molecule)
 st.write('SCScore of input molecule %.3f' % (score))
 
 m = Chem.MolFromSmiles(molecule)
-img = Draw.MolToImage(m,size=(1024,1024))
+img = Draw.MolToImage(m,size=(300,300))
 st.image(img)
+
+if score > 4.5:
+    st.markdown("![Alt Text](https://media2.giphy.com/media/qjr1ZNLWEVvg9aFCyL/giphy.gif?cid=ecf05e47f71kjwwqvqv3e97308uh7hogs2jca3wlp5s0t1xd&ep=v1_gifs_search&rid=giphy.gif&ct=g)")
